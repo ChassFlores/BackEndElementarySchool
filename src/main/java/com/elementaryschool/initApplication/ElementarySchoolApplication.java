18a375d4e5f31cf9fea71078952dba0a1aa41094
sflores <sflores@host.docker.internal>
2023-07-17 21:34:29 -0500

    Init Program

---
+++ src/main/java/com/elementaryschool/initApplication/ElementarySchoolApplication.java
@@ -0,0 +1,13 @@
+package com.elementaryschool.initApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementarySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementarySchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elementaryschool/initApplication/ElementarySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elementaryschool.initApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementarySchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
